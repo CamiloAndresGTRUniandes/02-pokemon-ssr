ad3ae91c10b483dc3de07879b340a0fb70e8e9bf
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2026-01-23 23:59:30 +0100

    fix deployment build issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,36 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
+import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
+import { Pokemons as PokemonService } from './pokemons/services/pokemons';
 
 export const serverRoutes: ServerRoute[] = [
+   {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'pricing',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Client,
+    getPrerenderParams: async () => {
+      const pokemonService = inject(PokemonService);
+      let pokemonIds: { id: string }[] = [];
+      await pokemonService.loadPage(1).subscribe(
+        pokemons => pokemons.map(p => {
+          pokemonIds.push({ id: p.id.toString() });
+        })
+      );
+      return pokemonIds;
+    }
+  },
   {
     path: '**',
     renderMode: RenderMode.Server
